ac93b0fcda35ba3c72f45832e7585ccc01c617d9
Lucas Moreno <73660766+lucash1881@users.noreply.github.com>
2023-04-18 09:45:29 +0200

    Add files via upload

---
+++ Proyecto_3/posts/views.py
@@ -0,0 +1,9 @@
+# posts/views.py
+from django.views.generic import ListView # new
+from .models import Post                  # new
+
+
+class HomePageView(ListView):             # new
+    model = Post                          # new
+    template_name = 'home.html'           # new
+    context_object_name = 'all_posts_list' 
